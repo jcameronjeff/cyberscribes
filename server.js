037cfeb1d5f9f63c5dfbc015ecc61c428feac45b
jcameronjeff <jcameronjeff@gmail.com>
2018-04-04 19:01:13 -0400

    built simplified webserver

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
-const express = require('express');
-const path =  require('path');
-const exphbs = require('express-handlebars');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-var app =  express();
+var express = require("express");
+var bodyParser = require("body-parser");
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
 
-app.set('port', (process.env.port || 3000));
+// Tells node that we are creating an "express" server
+var app = express();
 
-app.listen(app.get('port'). function(){
-  console.log('Server started on port' + app.get('port'))
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
-
-
-app.get('/', function(req, res)){
-   res.rend
-}
